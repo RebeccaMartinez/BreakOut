f04d175c5b1689f808f67736452b5ed2766c0d4a
rebecca <rebe.mart.gal@gmail.com>
2016-02-11 19:08:10 +0000

    creado el menu principal y el menu de niveles

---
+++ Assets/Scripts/LoadOnClickLM.cs
@@ -0,0 +1,10 @@
+﻿//Load de level menu when we click "Niveles"
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClickLM : MonoBehaviour {
+	public void LoadScene(){
+		SceneManager.LoadScene("Levels"); 
+	}
+}
---
+++ Assets/Scripts/LoadOnClickLevels.cs
@@ -0,0 +1,13 @@
+﻿//Load the corresponding level from the level menu
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LoadOnClickLevels : MonoBehaviour {
+
+	public void LoadScene(int l){
+		string level = string.Concat ("Level", l);
+		SceneManager.LoadScene(level); 
+	}
+}
