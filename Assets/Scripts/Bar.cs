ce2e69a89f88fb550edb8ff70ef462e648a43471
rebecca <rebe.mart.gal@gmail.com>
2016-03-29 17:49:49 +0100

    cambiada la forma de mover la barra

--- Assets/Scripts/Bar.cs
+++ Assets/Scripts/Bar.cs
@@ -7,12 +7,16 @@ using TETCSharpClient.Data;
 public class Bar : MonoBehaviour, IGazeListener{
 
 	//Variables
+	Vector3 speed = new Vector3(.5f, 0, 0);
 	public float barSpeed = 1f; //Velocidad de la barra
 	private Vector3 playerPos = new Vector3(0, -12f, 0); //Posición inicial
     //private Vector3 mousePosition;
+	Vector3 CamaraPosition;
+	float xPos;
 
 	void Start(){
 		//register for gaze updates
+		CamaraPosition = GameObject.Find ("Main Camera").transform.position;
 		GazeManager.Instance.AddGazeListener(this);
 
 	}
@@ -22,20 +26,33 @@ public class Bar : MonoBehaviour, IGazeListener{
 		Point2D gazeCoords = GazeDataValidator.Instance.GetLastValidSmoothedGazeCoordinates();
 		Debug.Log(gazeCoords);
 		Vector3 mouse;
+		playerPos = transform.position;
+		float radio = GetComponent<Renderer> ().bounds.extents.x;
 		if (null != gazeCoords) {
 			// Map gaze indicator
 			Point2D gp = UnityGazeUtils.GetGazeCoordsToUnityWindowCoords (gazeCoords);
-			mouse = new Vector3 ((float)gp.X, (float)gp.Y, 0);
+			mouse = new Vector3 ((float)gp.X, (float)gp.Y, CamaraPosition.z);
 			mouse = Camera.main.ScreenToWorldPoint(mouse);
 
 		} 
 		else {
 			mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			//float xPos = transform.position.x + (Input.GetAxis("Horizontal") * (barSpeed/2));
-			//Limita el movimiento de la barra en el eje x (para que no se salga de las paredes) y actualiza la posición
 		}
-		playerPos = new Vector3 (Mathf.Clamp (mouse.x, -18f, 18f), -12f, 0); 
-		transform.position = playerPos;
+
+		if (mouse.x - playerPos.x < -radio/100) { //Raton a la izquierda
+			transform.Translate(-1 * speed);
+		} 
+		else if(mouse.x - playerPos.x > radio/3){
+			transform.Translate (speed);
+		}
+			
+		if (transform.position.x > 18f) {
+			transform.position = new Vector3(18f, transform.position.y, transform.position.z);;
+		}
+		if (transform.position.x < -18f) {
+			transform.position = new Vector3(-18f, transform.position.y, transform.position.z);
+		}
+
 
     }
 
